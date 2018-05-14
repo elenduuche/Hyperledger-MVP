4a4313bb1fc2e326afaee00100bc497c3f2c80f0
Elendu Uche <eld.solution@gmail.com>
2018-05-14 17:59:50 +0100

    Updated base url

--- medichain-web/src/app/configuration.ts
+++ medichain-web/src/app/configuration.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Configuration {
-    public ApiIP = 'http://localhost';
+    public ApiIP = 'http://35.231.73.140';
     public ApiPort1 = '3000';
     public ApiPort2 = '3001';
     public Server1: string = this.ApiIP + ':' + this.ApiPort1;
